869819b8b2d4796ae369c2b26c217db27cb2e1c6
Graham Tang <airfree1452@gmail.com>
2016-03-10 22:40:17 +0800

    Create pieces

---
+++ src/Pieces/Elephants.java
@@ -0,0 +1,5 @@
+package Pieces;
+
+
+public class Elephants extends AbstractPieces {
+}
---
+++ src/Pieces/Rooks.java
@@ -0,0 +1,7 @@
+package Pieces;
+
+/**
+ * Created by airfr on 2016/3/10.
+ */
+public class Rooks extends AbstractPieces {
+}
